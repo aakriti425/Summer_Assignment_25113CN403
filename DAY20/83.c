ddef903a0c9180289e50b7d23a7f2e29458cb5cf
aakriti425 <paliwalaakriti3@gmail.com>
2026-06-18 23:49:32 +0530

    DAY Uploaded

--- DAY20/83.c
+++ DAY20/83.c
@@ -4,5 +4,23 @@
 #include<string.h>
 
 int main(){
+    // char str[50];
+    // printf("enter the string: ");
+    // scanf("%[^\n]", str);
+    // printf("string = %s \n", str);
     
+    // int i=0 , vowel=0 , consonants =0;
+    // while(str[i] != '\0'){
+    //     if(str[i] != 'a' || str[i] != 'e' || str[i] != 'i' || str[i] !='o' || str[i] != 'u' ||str[i] !='A' || str[i] !='E' || str[i] !='I' || str[i] !='O' || str[i] !='U'){
+    //         consonants ++;
+    //     } else {
+    //         vowel ++;
+    //     }
+
+    //     i++;
+    // }
+
+    // printf("Total no of Vowels = %d \n Total no of Consonants = %d ",vowel,consonants);
+    // return 0;
+
 }
